ff1769fc3d817b02721c3730529ecbfd4d49972f
Gabriela Marques <gabrielamaqu@gmail.com>
2023-10-03 11:36:56 -0300

    requisito 6

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,20 +1,16 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  const employee = data.employees.find((person) => person.id === id);
-  return !!employee && employee.managers.length > 0;
+  const employee = data.employees.find((emp) => emp.id === id);
+  return employee && employee.managers.length > 0;
 };
 
 const getRelatedEmployees = (managerId) => {
-  if (!isManager(managerId)) {
+  const employees = data.employees.filter((emp) => emp.managers.includes(managerId));
+  if (employees.length === 0) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-
-  const employees = data.employees
-    .filter((employee) => employee.managers.includes(managerId))
-    .map((employee) => `${employee.firstName} ${employee.lastName}`);
-
-  return employees;
+  return employees.map((emp) => `${emp.firstName} ${emp.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,56 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+const species = [
+  {
+    id: 'elephants',
+    residents: [
+      { name: 'Ilana', age: 12, location: 'NW', popularity: 3 },
+
+    ],
+  },
+];
+const elephantsId = 'elephants';
+
+describe('handlerElephants', () => {
+  it('deve retornar a quantidade de elefantes', () => {
+    const option = 'count';
+    const result = handlerElephants(option);
+    expect(result).toBe(4);
+  });
+
+  it('deve retornar um array com os nomes de todos os elefantes', () => {
+    const option = 'names';
+    const result = handlerElephants(option);
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(result).toEqual(expected);
+  });
+
+  it('deve retornar a média de idade dos elefantes', () => {
+    const option = 'averageAge';
+    const result = handlerElephants(option);
+    const expected = 10.5;
+    expect(result).toBeCloseTo(expected, 2);
+  });
+
+  it('deve retornar a localização dos elefantes', () => {
+    const option = 'location';
+    const result = handlerElephants(option);
+    expect(result).toEqual('NW');
+  });
+
+  it('deve retornar a popularidade dos elefantes', () => {
+    const option = 'popularity';
+    const result = handlerElephants(option);
+    const expected = 5; 
+    expect(result).toBe(expected);
+  });
+
+  it('deve retornar um array com os dias de disponibilidade dos elefantes', () => {
+    const option = 'availability';
+    const result = handlerElephants(option);
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(result).toEqual(expected);
+  });
+});
+
+
