7c22e0d985522c314ad885de27a79474761cac21
Luiz Paulino <lp.luizpaulino@gmail.com>
2023-10-04 22:18:29 -0300

    Mudanças

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -28,10 +28,43 @@ describe('getOpeningHours', () => {
     const result = () => getOpeningHours(day, time);
     expect(result).toThrowError('Invalid time format');
   });
+
   test('Hora não especificada', () => {
     const day = 'Wednesday';
     const time = null;
     const result = getOpeningHours(day, time);
     expect(result).toBe('The zoo is closed');
+  });
+
+  // Teste adicional: Zoológico está aberto em um dia específico e horário específico
+  test('Zoológico está aberto em um dia específico e horário específico', () => {
+    const day = 'Tuesday';
+    const time = '11:00 AM';
+    const result = getOpeningHours(day, time);
+    expect(result).toBe('The zoo is open');
+  });
+
+  // Teste adicional: Zoológico está fechado em um dia específico e horário específico
+  test('Zoológico está fechado em um dia específico e horário específico', () => {
+    const day = 'Wednesday';
+    const time = '8:00 PM';
+    const result = getOpeningHours(day, time);
+    expect(result).toBe('The zoo is closed');
+  });
+
+  // Teste adicional: Dia inválido
+  test('Dia inválido (minúsculas)', () => {
+    const day = 'sunday';
+    const time = '10:30 AM';
+    const result = () => getOpeningHours(day, time);
+    expect(result).toThrowError('The day must be valid. Example: Monday');
+  });
+
+  // Teste adicional: Hora válida (minúsculas)
+  test('Hora válida (minúsculas)', () => {
+    const day = 'Monday';
+    const time = '1:30 pm';
+    const result = getOpeningHours(day, time);
+    expect(result).toBe('The zoo is open');
   });
 });
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,53 +1,56 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('handlerElephants', () => {
-  it('deve retornar a quantidade de elefantes', () => {
-    const option = 'count';
-    const result = handlerElephants(option);
-    expect(result).toBe(4);
+  describe('handlerElephants', () => {
+    it('deve retornar a quantidade de elefantes', () => {
+      const option = 'count';
+      const result = handlerElephants(option);
+      expect(result).toBe(4);
+    });
+  
+    it('deve retornar um array com os nomes de todos os elefantes', () => {
+      const option = 'names';
+      const result = handlerElephants(option);
+      const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+      expect(result).toEqual(expected);
+    });
+  
+    it('deve retornar a média de idade dos elefantes', () => {
+      const option = 'averageAge';
+      const result = handlerElephants(option);
+      const expected = 10.5;
+      expect(result).toBeCloseTo(expected, 2);
+    });
+  
+    it('deve retornar a localização dos elefantes', () => {
+      const option = 'location';
+      const result = handlerElephants(option);
+      expect(result).toEqual('NW');
+    });
+  
+    it('deve retornar a popularidade dos elefantes', () => {
+      const option = 'popularity';
+      const result = handlerElephants(option);
+      const expected = 5;
+      expect(result).toBe(expected);
+    });
+  
+    it('deve retornar um array com os dias de disponibilidade dos elefantes', () => {
+      const option = 'availability';
+      const result = handlerElephants(option);
+      const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+      expect(result).toEqual(expected);
+    });
+  
+    it('deve lançar um erro para uma opção inválida', () => {
+      const option = 'invalidOption';
+      expect(() => handlerElephants(option)).toThrowError('Opção inválida');
+    });
+  
+    it('deve lançar um erro para opção vazia', () => {
+      const option = '';
+      expect(() => handlerElephants(option)).toThrowError('Opção inválida');
+    });
   });
-
-  it('deve retornar um array com os nomes de todos os elefantes', () => {
-    const option = 'names';
-    const result = handlerElephants(option);
-    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
-    expect(result).toEqual(expected);
-  });
-
-  it('deve retornar a média de idade dos elefantes', () => {
-    const option = 'averageAge';
-    const result = handlerElephants(option);
-    const expected = 10.5;
-    expect(result).toBeCloseTo(expected, 2);
-  });
-
-  it('deve retornar a localização dos elefantes', () => {
-    const option = 'location';
-    const result = handlerElephants(option);
-    expect(result).toEqual('NW');
-  });
-
-  it('deve retornar a popularidade dos elefantes', () => {
-    const option = 'popularity';
-    const result = handlerElephants(option);
-    const expected = 5;
-    expect(result).toBe(expected);
-  });
-
-  it('deve retornar um array com os dias de disponibilidade dos elefantes', () => {
-    const option = 'availability';
-    const result = handlerElephants(option);
-    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
-    expect(result).toEqual(expected);
-  });
-
-  it('deve lançar um erro para uma opção inválida', () => {
-    const option = 'invalidOption';
-    expect(() => handlerElephants(option)).toThrowError('Opção inválida');
-  });
-
-  it('deve lançar um erro para opção vazia', () => {
-    const option = '';
-    expect(() => handlerElephants(option)).toThrowError('Opção inválida');
-  });
+  
 });
